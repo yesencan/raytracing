8572281e711d19ec008607416e630004c3308c11
yunusemresencan <yunusemresencan0@gmail.com>
2023-11-23 12:17:10 +0300

    initial commit

---
+++ interval.h
@@ -0,0 +1,43 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+class interval {
+   public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {}  // Default interval is empty
+
+    interval(double _min, double _max) : min(_min), max(_max) {}
+
+    interval(const interval& a, const interval& b) : min(fmin(a.min, b.min)), max(fmax(a.max, b.max)) {}
+
+    double size() const { return max - min; }
+
+    interval expand(double delta) const {
+        auto padding = delta / 2;
+        return interval(min - padding, max + padding);
+    }
+
+    bool contains(double x) const { return min <= x && x <= max; }
+
+    bool surrounds(double x) const { return min < x && x < max; }
+
+    double clamp(double x) const {
+        if (x < min) return min;
+        if (x > max) return max;
+        return x;
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+
+interval operator+(const interval& ival, double displacement) {
+    return interval(ival.min + displacement, ival.max + displacement);
+}
+
+interval operator+(double displacement, const interval& ival) { return ival + displacement; }
+
+#endif
